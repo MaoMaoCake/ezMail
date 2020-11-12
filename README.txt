eb00ddcdfd08635774be611fa344f886748e2843
maomao <tangpansak@gmail.com>
2020-11-12 10:19:16 +0700

    add packgage files

---
+++ README.txt
@@ -0,0 +1,2 @@
+Email Wrapper written with email library
+Made to be easy to send emails
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Information Technology',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='ezMail',
+    version='0.0.1',
+    description='Easy to use python mailing app',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Jirapong Pansak',
+    author_email='me@jirapongpansak.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='emails',
+    packages=find_packages(),
+    install_requires=['']
+)
